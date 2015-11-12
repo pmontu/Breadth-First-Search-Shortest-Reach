1e9012fed1efda42830d5e672fd6f6b7e6e4bef9
Manoj Kumar P <manoj.k@plancess.com>
2015-11-12 18:53:00 +0530

    bingo

--- hackerrank_bfs_shortest_reach.py
+++ hackerrank_bfs_shortest_reach.py
@@ -1,46 +1,41 @@
-import sys
-
-def fwd(S):
-    nodes = [idx+1 for idx, x in enumerate(eds[S-1]) if x==1]
-    n = None
-    for n in nodes:
-        existing = SD[n-1]
-        if existing != sys.maxint:
-            SD[n-1] = min(SD[S-1] + 1, SD[n-1])
-        else:
-            SD[n-1] = SD[S-1] + 1
-    for n in nodes:
-        fwd(n)
-
-def rev(S):
-    nodes = []
-    for idx, l in enumerate(eds):
-        if l[S-1] == 1:
-            nodes.append(idx+1)
-    for n in nodes:
-        existing = SD[n-1]
-        if existing != sys.maxint:
-            SD[n-1] = min(SD[S-1] + 1, SD[n-1])
-        else:
-            SD[n-1] = SD[S-1] + 1
-    for n in nodes:
-        rev(n)
-
-
 f = open("input01.txt")
 T = int(f.readline())
 for test in range(T):
     N, M = map(int, f.readline().split(' '))
-    eds = []
-    for i in range(N): eds.append([0]*N)
+    eds = {}
     for i in range(M):
         n1,n2 = map(int, f.readline().split(' '))
-        eds[n1-1][n2-1] = 1
+        if n1 in eds:
+            if n2 not in eds[n1]:
+                eds[n1].append(n2)
+        else:
+            eds[n1] = [n2]
+        if n2 in eds:
+            if n1 not in eds[n2]:
+                eds[n2].append(n1)
+        else:
+            eds[n2] = [n1]
     S = int(f.readline())
-    SD = [sys.maxint] * N
-    SD[S-1] = 0
-    fwd(S)
-    rev(S)
-    del SD[S-1]
-    SD = [str(sd*6) if sd!=sys.maxint else "-1" for sd in SD]
-    print " ".join(SD)
+
+    visited = [False] * N
+    queue = []
+    queue.append(S)
+    visited[S-1] = True
+    level = 0
+
+    import sys
+    distance = [sys.maxint] * N
+    distance[S-1] = 0
+
+    while(queue):
+        s = queue.pop(0)
+        if s in eds:
+            for t in eds[s]:
+                if not visited[t-1]:
+                    visited[t-1] = True
+                    queue.append(t)
+                    distance[t-1] = distance[t-1] + distance[s-1] + 1 if distance[t-1] != sys.maxint else distance[s-1] + 1
+
+    del distance[S-1]
+    distance = [str(x*6) if x!=sys.maxint else "-1" for x in distance]
+    print " ".join(distance)
