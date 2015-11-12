1a961e8784996dae48e99033d0a7ef7d3f9d97b9
Manoj Kumar P <manoj.k@plancess.com>
2015-11-12 17:50:02 +0530

    matrix & recursion

--- hackerrank_bfs_shortest_reach.py
+++ hackerrank_bfs_shortest_reach.py
@@ -1,6 +1,6 @@
 import sys
 
-def run(S):
+def fwd(S):
     nodes = [idx+1 for idx, x in enumerate(eds[S-1]) if x==1]
     n = None
     for n in nodes:
@@ -9,11 +9,25 @@ def run(S):
             SD[n-1] = min(SD[S-1] + 1, SD[n-1])
         else:
             SD[n-1] = SD[S-1] + 1
-    print S, SD
     for n in nodes:
-        run(n)
+        fwd(n)
 
-f = open("input02.txt")
+def rev(S):
+    nodes = []
+    for idx, l in enumerate(eds):
+        if l[S-1] == 1:
+            nodes.append(idx+1)
+    for n in nodes:
+        existing = SD[n-1]
+        if existing != sys.maxint:
+            SD[n-1] = min(SD[S-1] + 1, SD[n-1])
+        else:
+            SD[n-1] = SD[S-1] + 1
+    for n in nodes:
+        rev(n)
+
+
+f = open("input01.txt")
 T = int(f.readline())
 for test in range(T):
     N, M = map(int, f.readline().split(' '))
@@ -23,8 +37,10 @@ for test in range(T):
         n1,n2 = map(int, f.readline().split(' '))
         eds[n1-1][n2-1] = 1
     S = int(f.readline())
-    for l in eds: print l
-    print S
     SD = [sys.maxint] * N
     SD[S-1] = 0
-    run(S)
+    fwd(S)
+    rev(S)
+    del SD[S-1]
+    SD = [str(sd*6) if sd!=sys.maxint else "-1" for sd in SD]
+    print " ".join(SD)
