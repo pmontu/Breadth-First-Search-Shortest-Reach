0fbaaf323765b3024572032fd12461883f0c802d
Manoj Kumar P <manoj.k@plancess.com>
2015-11-12 17:19:34 +0530

    forward

--- hackerrank_bfs_shortest_reach.py
+++ hackerrank_bfs_shortest_reach.py
@@ -1,3 +1,18 @@
+import sys
+
+def run(S):
+    nodes = [idx+1 for idx, x in enumerate(eds[S-1]) if x==1]
+    n = None
+    for n in nodes:
+        existing = SD[n-1]
+        if existing != sys.maxint:
+            SD[n-1] = min(SD[S-1] + 1, SD[n-1])
+        else:
+            SD[n-1] = SD[S-1] + 1
+    print S, SD
+    for n in nodes:
+        run(n)
+
 f = open("input02.txt")
 T = int(f.readline())
 for test in range(T):
@@ -9,39 +24,7 @@ for test in range(T):
         eds[n1-1][n2-1] = 1
     S = int(f.readline())
     for l in eds: print l
-    #run(N, eds, S)
-
-def run(N, eds, S):
-    bfs_ar = [[S]]
-    weight = 6
-    def calc(bfs_ar):
-        new_bfs_ar = []
-        isChanged = False
-        for bfs in bfs_ar:
-            search_node = bfs[-1]
-            n = set([ed[1] for ed in eds if ed[0]==search_node])
-            i = _not_assigned = object()
-            for i in n:
-                new_bfs_ar.append(bfs + [i])
-                isChanged = True
-            if i is _not_assigned:
-                new_bfs_ar.append(bfs)
-        if isChanged:
-            bfs_ar = new_bfs_ar
-            return bfs_ar
-        else: return None
-
-    while True:
-        t = calc(bfs_ar)
-        if not t:
-            break
-        bfs_ar = t
-
-    import sys
-    pos = [sys.maxint] * N
-    for bfs in bfs_ar:
-        for idx, n in enumerate(bfs):
-            pos[n-1] = min(pos[n-1], idx*weight)
-    pos = [str(x) if x != sys.maxint else "-1" for x in pos]
-    del pos[S-1]
-    print " ".join(pos)
+    print S
+    SD = [sys.maxint] * N
+    SD[S-1] = 0
+    run(S)
